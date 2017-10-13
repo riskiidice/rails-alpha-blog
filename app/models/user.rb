ce4bb9551593f3d384b168034dcf3d6c1cd44da1
Tikumporn Wankvar <tikumporn@Macintosh.local>
2017-10-13 10:12:00 +0700

    Adding user admin and restriction for admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :articles
+  has_many :articles, dependent: :destroy
   before_save {self.email = email.downcase}
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 6, maximum: 20}
   VALID_EMAIL_REGEX =  /\A[\w+\-\.\_]+@[a-z\d\-\.\_]+[a-z]+\z/i
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008063816) do
+ActiveRecord::Schema.define(version: 20171013021851) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20171008063816) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
